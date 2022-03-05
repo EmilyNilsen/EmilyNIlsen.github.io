874fce81e3adca00d11f70d0efa07dc0773154ac
biancanilsen <nilsen.loli.emily@gmail.com>
2022-03-05 16:42:47 -0300

    iniciando projects

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Projects() {
+    return(
+        <section id='projects' className='container-projects'>
+            <h3 className="title-projects">Projetos</h3>
+            <div className="project-content">
+                <div class="project-grid">
+                    <div class="project-list">
+                    </div>
+                    <div class="project-list">
+                    </div>
+                    <div class="project-list">
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
