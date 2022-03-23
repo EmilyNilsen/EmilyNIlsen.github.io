c55821451f3fa395638650df0d59676df5f8e93f
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-03-23 19:15:40 -0300

    adicionando imagem aos projetos

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import image from "../images/computador.png";
+import originSixImage from "../images/imageSite2.jpeg";
+import storeManagerImage from "../images/storeManager.jpeg";
+import trivia from "../images/trivia.jpeg";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -27,36 +30,42 @@ export default function Projects() {
           <SwiperSlide>
             <div className="slide-list">
               <img src={image} alt="computador" className="image-computador" />
+              <img src={trivia} alt="imagem da aplicação Trivia" className="image-site" />
               <div className="container-text-projects">
                 <h3>Projeto Trivia React Redux</h3>
                 <p>
                 Projeto em grupo proposto pelo curso da Trybe em que deveria ser criado um jogo de perguntas e respostas baseado no jogo Trivia (tipo um show do milhão americano) utilizando React e Redux.
                 </p>
-                <h6>Repositório do projeto: https://github.com/EmilyNilsen/project-trivia-react-redux</h6>
+                <h6>Repositório do projeto <a href="https://github.com/EmilyNilsen/project-trivia-react-redux" target="_blank"
+            rel="noreferrer">aqui</a>!</h6>
               </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="slide-list">
               <img src={image} alt="computador" className="image-computador" />
+              <img src={storeManagerImage} alt='imagem da aplicação StoreManager' className="image-site" />
               <div className="container-text-projects">
                 <h3>Projeto Store Manager </h3>
                 <p>
                 Projeto proposto pelo curso da Trybe, em que foi desenvolvido uma API de um CRUD (Create, Read, Update e Delete) utilizando a arquitetura MSC. Foi desenvolvido um sistema de gerenciamento de vendas, onde será possível criar, visualizar, deletar, atualizar produtos e vendas e mais de 40% de cobertura de testes unitários.
                 </p>
-                <h6>Repositório do projeto: https://github.com/EmilyNilsen/project-store-manager</h6>
+                <h6>Repositório do projeto <a href=" https://github.com/EmilyNilsen/project-store-manager" target="_blank"
+            rel="noreferrer">aqui</a>!</h6>
               </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="slide-list">
               <img src={image} alt="computador" className="image-computador" />
+              <img src={originSixImage} alt='imagem site origin six' className="image-site" />
               <div className="container-text-projects">
                 <h3>Projeto OriginSix</h3>
                 <p>
-                OriginSix é uma página institucional no formato One Page, responsiva, para usar em diversos tipos de micro, pequena e média empresas. Contém as seguintes seções: Header, Navigation, Home, Sobre, Serviços, Depoimentos, Contato e Footer. Projeto foi executado junto as aulas disponíveis na plataforma da Rocketseat.
+                OriginSix é uma página institucional no formato SPA, responsiva, para usar em diversos tipos de micro, pequena e média empresas. Contém as seguintes seções: Header, Navigation, Home, Sobre, Serviços, Depoimentos, Contato e Footer. Projeto foi executado junto as aulas disponíveis na plataforma da Rocketseat.
                 </p>
-                <h6>Repositório do projeto: https://github.com/EmilyNilsen/nlw_origin</h6>
+                <h6>Repositório do projeto <a href="https://github.com/EmilyNilsen/nlw_origin" target="_blank"
+            rel="noreferrer">aqui</a>!</h6>
               </div>
             </div>
           </SwiperSlide>
